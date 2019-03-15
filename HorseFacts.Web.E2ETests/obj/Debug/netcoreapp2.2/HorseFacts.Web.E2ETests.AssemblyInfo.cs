923e04356e9ae7ce69d69140afb7d5456158cf00
Richard Foster <richard@rpf.me>
2019-03-15 13:42:00 +0000

    Add GetRandomQuestionableHorseFact use case

--- HorseFacts.Web.E2ETests/obj/Debug/netcoreapp2.2/HorseFacts.Web.E2ETests.AssemblyInfo.cs
+++ HorseFacts.Web.E2ETests/obj/Debug/netcoreapp2.2/HorseFacts.Web.E2ETests.AssemblyInfo.cs
@@ -7,7 +7,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("HorseFacts.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "/Users/richard/projects/HorseFacts/HorseFacts.Web", "HorseFacts.Web.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("HorseFacts.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "/Users/david/Projects/horse-facts/HorseFacts.Web", "HorseFacts.Web.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HorseFacts.Web.E2ETests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
