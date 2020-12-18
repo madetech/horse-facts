42a863fb691208250aad7ecd712b18dda43bca8b
Vivian <vivian.roberts@madetech.com>
2020-12-18 18:00:10 +0000

    Make english better

--- HorseFacts.Core.Tests/UseCases/GetRandomVeganIpsumParagraph.Tests.cs
+++ HorseFacts.Core.Tests/UseCases/GetRandomVeganIpsumParagraph.Tests.cs
@@ -10,7 +10,7 @@ namespace HorseFacts.Core.Tests.UseCases
 {
     public class GetRandomVeganIpsumParagraphTests : IFoodIpsumGateway
     {
-        private string _paragraph;
+        private string _sentence;
         private readonly GetRandomVeganIpsumParagraph _subject;
 
         public GetRandomVeganIpsumParagraphTests()
@@ -21,17 +21,17 @@ namespace HorseFacts.Core.Tests.UseCases
         [Fact]
         public async Task Sentence_should_be_unchanged_when_there_are_no_meat_words()
         {
-            _paragraph = "Suspendisse nulla erat, interdum nec est laoreet, gravida iaculis nunc";
+            _sentence = "Suspendisse nulla erat, interdum nec est laoreet, gravida iaculis nunc";
 
             var result = await _subject.Execute();
 
-            result.VeganIpsumParagraph.Should().Be(_paragraph);
+            result.VeganIpsumParagraph.Should().Be(_sentence);
         }
 
         [Fact]
-        public async Task Sentence_meat_words_should_be_replaced_with_vegetable_words()
+        public async Task Meat_words_should_be_replaced_with_vegetable_words()
         {
-            _paragraph = "Ex dolor velit proident swine. Dolor fatback hamburger ut tail ribeye esse pork loin cillum";
+            _sentence = "Ex dolor velit proident swine. Dolor fatback hamburger ut tail ribeye esse pork loin cillum";
             var expected = "Ex dolor velit proident banana. Dolor banana banana ut banana banana esse banana cillum";
 
             var result = await _subject.Execute();
@@ -40,9 +40,9 @@ namespace HorseFacts.Core.Tests.UseCases
         }
 
         [Fact]
-        public async Task Sentence_plural_meat_words_should_be_replaced_with_plural_vegetable_words()
+        public async Task Plural_meat_words_should_be_replaced_with_plural_vegetable_words()
         {
-            _paragraph = "Ex dolor velit proident swines. Dolor fatbacks hamburgers ut tails ribeyes esse short loins cillum";
+            _sentence = "Ex dolor velit proident swines. Dolor fatbacks hamburgers ut tails ribeyes esse short loins cillum";
             var expected = "Ex dolor velit proident bananas. Dolor bananas bananas ut bananas bananas esse bananas cillum";
 
             var result = await _subject.Execute();
@@ -51,9 +51,9 @@ namespace HorseFacts.Core.Tests.UseCases
         }
 
         [Fact]
-        public async Task Sentence_capital_case_meat_words_should_be_replaced_with_capital_case_vegetable_words()
+        public async Task Capital_case_meat_words_should_be_replaced_with_capital_case_vegetable_words()
         {
-            _paragraph = "Ex dolor velit proident Swines. Dolor Fatback Hamburgers ut Tails Ribeye esse Short loins cillum";
+            _sentence = "Ex dolor velit proident Swines. Dolor Fatback Hamburgers ut Tails Ribeye esse Short loins cillum";
             var expected = "Ex dolor velit proident Bananas. Dolor Banana Bananas ut Bananas Banana esse Bananas cillum";
 
             var result = await _subject.Execute();
@@ -65,7 +65,7 @@ namespace HorseFacts.Core.Tests.UseCases
         {
             return Task.FromResult(new FoodIpsum
             {
-                Paragraph = _paragraph
+                Paragraph = _sentence
             });
         }
     }
